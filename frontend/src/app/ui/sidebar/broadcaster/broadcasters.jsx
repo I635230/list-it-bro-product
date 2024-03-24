1cbe0b6b7405a0aeb6417a8592e45f82be4ee32e
I635230 <yugo.susuki@gmail.com>
2024-03-25 04:51:35 +0900

    :+1: renewが2回実行される問題の解決

--- frontend/src/app/ui/sidebar/broadcaster/broadcasters.jsx
+++ frontend/src/app/ui/sidebar/broadcaster/broadcasters.jsx
@@ -1,24 +1,24 @@
 'use client'
 
 import Broadcaster from '@/app/ui/sidebar/broadcaster/broadcaster'
-import { redirect } from 'next/navigation'
 import { useAtom } from 'jotai'
 import { loginState } from '@/app/state/login'
+import { useRouter } from 'next/navigation'
 
 export default function Broadcasters({ broadcasters }) {
+  const router = useRouter()
+
   // ログイン状態をヘッダーと共有
   const [isLogin] = useAtom(loginState)
 
   // broadcastersの取得でUnauthorizedだったら、トークンを更新する
   if (broadcasters?.status == 'Unauthorized') {
-    // TODO
-    console.log('renewへのリダイレクト')
-    // redirect('/renew')
+    router.push('/renew')
   }
 
   return (
     <>
-      {isLogin && (
+      {isLogin && broadcasters?.status != 'Unauthorized' && (
         <>
           {broadcasters?.map((broadcaster, index) => (
             <Broadcaster broadcaster={broadcaster} key={index} />
