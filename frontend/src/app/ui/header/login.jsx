34de714b52d76d522dbfd7c8d2d026bcf8398af6
I635230 <yugo.susuki@gmail.com>
2024-03-26 21:43:01 +0900

    :+1: 環境変数の設定

--- frontend/src/app/ui/header/login.jsx
+++
@@ -1,9 +0,0 @@
-export default function Login() {
-  return (
-    <a
-      href={`https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_BASE_URL}/login&scope=user:read:follows`}
-    >
-      ログイン
-    </a>
-  )
-}
