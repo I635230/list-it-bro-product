dddd1c3d69d1dcb1c8b24d269c733ee7bf645279
I635230 <yugo.susuki@gmail.com>
2025-06-27 22:48:09 +0900

    :+1: update-app-access-tokenの追加と適用

--- backend/bin/.run_jobs.sh
+++ backend/bin/.run_jobs.sh
@@ -4,3 +4,4 @@
 cd /var/www/list-it-bro-product/backend && bundle exec rails runner -e production 'Batch::Clips.update_all' >> /var/www/list-it-bro-product/backend/log/cron.log 2>&1
 cd /var/www/list-it-bro-product/backend && bundle exec rails runner -e production 'Batch::Rankings.destroy' >> /var/www/list-it-bro-product/backend/log/cron.log 2>&1
 cd /var/www/list-it-bro-product/backend && bundle exec rails runner -e production 'Batch::Rankings.create' >> /var/www/list-it-bro-product/backend/log/cron.log 2>&1
+cd /var/www/list-it-bro-product/backend && bundle exec rails runner -e production 'Batch::DealAppAccessToken.update' >> /var/www/list-it-bro-product/backend/log/cron.log 2>&1
